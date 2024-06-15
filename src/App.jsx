e1ccc849e6fa15795ef4b55586c47f2a2e9bfc48
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2024-06-16 00:31:50 +0530

    Update App.jsx
    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import Login from './pages/Login'
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
