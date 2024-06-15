7fbc32a34419192002b875586cc981a87abc9b43
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2024-06-16 00:22:58 +0530

    updated
    updated

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login'
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import LeaveManagement from './pages/LeaveManagement';
+import MarkAttendance from './pages/MarkAttendance';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+            <ToastContainer />
+                <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/leave-management" element={<LeaveManagement />} />
+                <Route path="/mark-attendance" element={<MarkAttendance />} />
+                    {/* Add more routes for other pages as needed */}
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
